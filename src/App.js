24790ac3fae1f5e1f38783adf78c93b7fca4cef8
Dare3 <enitan_segun@yahoo.co.uk>
2024-09-23 10:50:39 -0400

    rendering deck

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Deck from "./Deck"; // Import the Deck component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Deck of Cards</h1>
+      <Deck /> {/* Render the Deck component */}
     </div>
   );
 }
